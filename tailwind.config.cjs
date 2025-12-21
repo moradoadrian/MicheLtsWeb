32557e30683578ed1c96d2e187e0d9beca3deb07
Adrián Morado <moradoadrian@gmail.com>
2025-12-21 11:39:16 -0600

    Miche Lts Web

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					DEFAULT: '#00cf6a',
+					dark: '#00a455'
+				},
+				bg: '#0b0b0b'
+			},
+			fontFamily: {
+				sans: ['Outfit', 'sans-serif'],
+				serif: ['"Playfair Display"', 'serif'],
+			}
+		}
+	},
+	plugins: []
+};
